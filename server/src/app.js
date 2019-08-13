40621e55ed9203847b7647f39f512f2c20863467
misgana96 <misganayoseph@gmail.com>
2019-08-12 18:53:02 -0700

    an application using express server

---
+++ server/src/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const cors =require('cors')
+const bodyParse = require('body-parser')
+const morgan = require('morgan')
+
+
+const app = express()
+app.use(morgan('combine'))
+app.use(bodyParse.json())
+app.use(cors())
+
+app.get('/status', (req, res)=>{
+	res.send({
+		message: 'hello world'
+	})
+})
+app.listen(process.env.PORT || 8081)
+
+
+
+
